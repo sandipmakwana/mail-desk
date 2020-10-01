5f643bf66b5a22cf006ee88838d19343d40b6ab2
sandipmakwana <sandy007.makwana@gmail.com>
2020-10-01 13:38:36 -0400

    Delete 09_01_19_user_fields.sql

--- sql_files/09_01_19_user_fields.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `vs_emp_details`
-ADD `emp_locationid` varchar(50) COLLATE 'latin1_swedish_ci' NOT NULL AFTER `emp_lastname`;
-ALTER TABLE `vs_emp_details`
-ADD `emp_departmentid` varchar(250) COLLATE 'latin1_swedish_ci' NOT NULL AFTER `emp_locationid`; 
